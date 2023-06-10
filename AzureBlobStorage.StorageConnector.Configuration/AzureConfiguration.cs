18b02abd29f129a8b11e836bf1fe9b078b61e3e7
OldSinner <antylifepatia@gmail.com>
2023-06-11 01:44:31 +0200

    Cleaning after try use PageBlob

--- AzureBlobStorage.StorageConnector.Configuration/AzureConfiguration.cs
+++ AzureBlobStorage.StorageConnector.Configuration/AzureConfiguration.cs
@@ -4,6 +4,5 @@ namespace AzureBlobStorage.StorageConnector.Configuration
     {
         public string ConnectionString { get; set; } = string.Empty;
         public string ContainerName { get; set; } = string.Empty;
-        public int PageSize { get; set; }
     }
 }
